6adccbed0973ab11fa61871a3df950d2a139f68d
Ahmed Ali <ahmedcali1984@gmail.com>
2025-02-21 17:46:26 +0300

    Indent properly

--- alog.h
+++ alog.h
@@ -5,31 +5,31 @@
 #include <stdarg.h>
 
 typedef enum {
-	INFO,
-	ERROR,
-	DEBUG,
-	WARNING,
+    INFO,
+    ERROR,
+    DEBUG,
+    WARNING,
 } Log_Levels;
 
 #define LOGDEF static
 
 LOGDEF const char *levels_as_cstr(Log_Levels level) {
-	switch (level) {
-		case ERROR: return "[ERROR]: ";
-		case DEBUG: return "[DEBUG]: ";
-		case WARNING: return "[WARNING]: ";
-		case INFO:
-		default: return "[INFO]: ";
-	}
+    switch (level) {
+        case ERROR: return "[ERROR]: ";
+        case DEBUG: return "[DEBUG]: ";
+        case WARNING: return "[WARNING]: ";
+        case INFO:
+        default: return "[INFO]: ";
+    }
 }
 
 LOGDEF void Logging(FILE *stream, Log_Levels level, const char *fmt, ...) {
-	fprintf(stream, "%s" , levels_as_cstr(level));
+    fprintf(stream, "%s" , levels_as_cstr(level));
 
-	va_list args;
-	va_start(args, fmt);
-	vfprintf(stream, fmt, args);
-	va_end(args);
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stream, fmt, args);
+    va_end(args);
 }
 
 #define Log_Out(level, fmt, ...) Logging(stdout, level, fmt, ##__VA_ARGS__)
